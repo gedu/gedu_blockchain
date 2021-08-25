18154e8b0025f4e5ba1b45967b08cf07d471ddd1
Edu <>
2021-08-25 18:08:12 -0300

    clean up + height and time

--- private_blockchain/src/models/blockchain.js
+++ private_blockchain/src/models/blockchain.js
@@ -13,13 +13,23 @@ const Block = require("./block");
 class Blockchain {
   constructor() {
     this.chain = [];
-    this.addBlock(new Block("Genesis Block"));
+    this.addBlock(createGenesisBlock());
+  }
+
+  createGenesisBlock() {
+    return new Block("Genesis Block");
+  }
+
+  getLatestBlock() {
+    return this.chain[this.chain.length - 1];
   }
 
   addBlock(newBlock) {
     if (this.chain.length > 0) {
-      newBlock.previousBlockHash = this.chain[this.chain.length - 1].hash;
+      newBlock.previousBlockHash = this.getLatestBlock().hash;
     }
+    newBlock.height = this.chain.length;
+    newBlock.time = new Date().getTime().toString().slice(0, -3); //UTC format
     newBlock.generateHash();
     this.chain.push(newBlock);
   }
