eb7c1cb83b78f4b7aef1acf2a3e0e86c8e4a005c
Edu <>
2021-08-27 09:04:08 -0300

    renamed

---
+++ get_started_concepts/src/utils/imageEncodeDecode.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const path = require("path");
+
+function encodeImage(pathToFile) {
+  const imageBuffer = fs.readFileSync(path.resolve(pathToFile));
+  const imageEncoded = Buffer.from(imageBuffer).toString("hex");
+  return imageEncoded;
+}
+
+function decodeImage(encodedFile) {
+  const pathFile = "./src/assets/test-pattern-decoded.jpg";
+  const fileHex = Buffer.from(encodedFile, "hex");
+  fs.writeFileSync(path.resolve(pathFile), fileHex);
+}
